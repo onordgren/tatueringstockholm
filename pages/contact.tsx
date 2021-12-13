d9504613cb603fb1a652fbb3d1ca2967b08dc071
Otto Nordgren <>
2021-12-13 14:32:51 +0100

    form fixes

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -4,7 +4,7 @@ import Textarea from '../components/Textarea';
 const Contact = () => {
   return (
     <div>
-      <form name="contact" data-netlify="true">
+      <form name="contact" method="POST" data-netlify="true">
         <Input type="text" name="name" label="Name" />
         <Input type="email" name="email" label="Email" />
         <Input type="tel" name="phone" label="Phone number" />
