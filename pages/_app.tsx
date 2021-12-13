6e6b38f9dad46e35df20b139ecd814f6cf8b2451
Otto Nordgren <>
2021-12-13 15:03:41 +0100

    netlify cms

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Header from '../components/Header'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></Script>
       <Header />
       <Component {...pageProps} />
     </div>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
